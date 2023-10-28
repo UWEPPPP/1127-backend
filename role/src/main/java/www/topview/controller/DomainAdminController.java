b23aaf078045f47cf8bc556aef7db1e01f4088cd
liu <a2506991786@qq.com>
2023-10-28 19:19:38 +0800

    feat/weidentity-init

--- role/src/main/java/www/topview/controller/DomainAdminController.java
+++ role/src/main/java/www/topview/controller/DomainAdminController.java
@@ -1,14 +1,22 @@
 package www.topview.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import www.topview.result.CommonResult;
+import www.topview.service.DomainService;
+import www.topview.service.WeIdentityService;
+
+import javax.swing.text.StyledEditorKit;
 
 @RestController
 @RequestMapping("/api/domainAdmin")
 @ControllerAdvice
 public class DomainAdminController {
+    @Autowired
+    DomainService domainService;
     @PostMapping("/addCompany")
     public void addCompany() {
 
@@ -22,6 +30,17 @@ public class DomainAdminController {
     @PostMapping("/deleteCompany")
     public void deleteCompany() {
 
+    }
+
+    @PostMapping("/registerCpt")
+    public CommonResult<Boolean> registerCpt() {
+       // return CommonResult.operateSuccess("Register cpt Success",domainService.registerCpt());
+       return null;
+    }
+
+    @PostMapping("/getCptTemplate")
+    public CommonResult<String> getCptTemplate() {
+        return CommonResult.operateSuccess("Get template Success",domainService.getCptTemplate());
     }
 
 }
