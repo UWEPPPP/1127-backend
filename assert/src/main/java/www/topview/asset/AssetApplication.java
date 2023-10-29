4c3f69136055f284cec49951493ecbd1a3d3827d
lql <1964757607@qq.com>
2023-10-29 14:37:12 +0800

    init(asset):初始化asset模块以及配置

--- assert/src/main/java/www/topview/asset/AssetApplication.java
+++ assert/src/main/java/www/topview/asset/AssetApplication.java
@@ -1,5 +1,6 @@
 package www.topview.asset;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lql
  * @date 2023/10/29
  */
+@MapperScan("com.topview.asset.mapper")
 @SpringBootApplication
 public class AssetApplication {
     public static void main(String[] args) {
