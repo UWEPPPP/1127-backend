557764491141d233d9703b2e036fb2821f1ea291
lql <1964757607@qq.com>
2023-10-29 14:06:51 +0800

    feat(asset):初始化asset模块以及配置

---
+++ assert/src/main/java/www/topview/asset/AssetApplication.java
@@ -0,0 +1,15 @@
+package www.topview.asset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lql
+ * @date 2023/10/29
+ */
+@SpringBootApplication
+public class AssetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AssetApplication.class,args);
+    }
+}
