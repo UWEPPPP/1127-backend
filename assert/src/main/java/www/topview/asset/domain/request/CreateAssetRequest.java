f979e691024d2adc14a29b3a629c7be829ec0733
lql <1964757607@qq.com>
2023-10-30 14:50:11 +0800

    feat(asset.request):添加创建资产的请求类

--- assert/src/main/java/www/topview/asset/domain/request/CreateAssetRequest.java
+++ assert/src/main/java/www/topview/asset/domain/request/CreateAssetRequest.java
@@ -1,6 +1,7 @@
 package www.topview.asset.domain.request;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 创建asset
@@ -10,5 +11,23 @@ import lombok.Data;
  */
 @Data
 public class CreateAssetRequest {
+    /**
+     * 文件
+     */
+    private MultipartFile file;
 
+    /**
+     * 资产名
+     */
+    private String name;
+
+    /**
+     * 组名（为空则为default）
+     */
+    private String groupName;
+
+    /**
+     * 资产描述
+     */
+    private String description;
 }
