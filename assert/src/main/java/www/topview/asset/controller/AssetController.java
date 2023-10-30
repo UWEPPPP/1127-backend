d984861715317ef0f0248b739cf5db725081972e
lql <1964757607@qq.com>
2023-10-30 18:59:44 +0800

    refactor(bo):修改名字

--- assert/src/main/java/www/topview/asset/controller/AssetController.java
+++ assert/src/main/java/www/topview/asset/controller/AssetController.java
@@ -4,7 +4,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import www.topview.asset.domain.dto.CreateAssetDTO;
 import www.topview.asset.domain.vo.AssetDetailsVO;
-import www.topview.asset.domain.request.CreateAssetRequest;
+import www.topview.asset.domain.bo.CreateAssetBO;
 import www.topview.asset.service.AssetService;
 import www.topview.result.CommonResult;
 
@@ -23,16 +23,16 @@ public class AssetController {
     private AssetService assetService;
 
     @PostMapping("/")
-    public CommonResult<Boolean> create(@Validated @RequestBody CreateAssetRequest createAssetRequest){
+    public CommonResult<Boolean> create(@Validated @RequestBody CreateAssetBO createAssetBO){
         CreateAssetDTO createAssetDTO = new CreateAssetDTO();
         // TODO 通过token获取userID
         createAssetDTO.setUserId(1L);
-        createAssetDTO.setCreateAssetRequest(createAssetRequest);
+        createAssetDTO.setCreateAssetBO(createAssetBO);
         return CommonResult.operateSuccess("创建资产成功",assetService.createAsset(createAssetDTO));
     }
 
     @GetMapping("/{id}")
     public CommonResult<AssetDetailsVO> getAsset(@PathVariable Integer id){
-        return null;
+        return CommonResult.operateSuccess("查询成功",assetService.getAssetMessage(id));
     }
 }
