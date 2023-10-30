a152bf86f9835b719547fd1fd7eb3052c1fa9444
lql <1964757607@qq.com>
2023-10-30 19:57:51 +0800

    feat(mapper):添加查询有效的资产列表方法

--- assert/src/main/java/www/topview/asset/mapper/AssetMapper.java
+++ assert/src/main/java/www/topview/asset/mapper/AssetMapper.java
@@ -1,8 +1,11 @@
 package www.topview.asset.mapper;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import www.topview.asset.domain.po.Asset;
+
+import java.util.List;
 
 /**
  * 资产类的mapper
@@ -11,4 +14,11 @@ import www.topview.asset.domain.po.Asset;
  * @date 2023/10/29
  */
 public interface AssetMapper extends BaseMapper<Asset> {
+    /**
+     * 查询有效的资产列表
+     * @return 返回有效的资产列表
+     */
+    default List<Asset> selectValidList(){
+        return this.selectList(new QueryWrapper<Asset>().eq("status",0));
+    }
 }
