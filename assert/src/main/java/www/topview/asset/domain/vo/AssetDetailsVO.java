978c712cf19dac46ce1e40a63b0bbba5d62d825e
lql <1964757607@qq.com>
2023-10-30 19:26:15 +0800

    feat(vo):添加获取单个资产的详细信息

---
+++ assert/src/main/java/www/topview/asset/domain/vo/AssetDetailsVO.java
@@ -0,0 +1,70 @@
+package www.topview.asset.domain.vo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * 用于显示资产详细信息的vo
+ *
+ * @author lql
+ * @date 2023/10/30
+ */
+@Data
+@Accessors(chain = true)
+public class AssetDetailsVO {
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * 资产名字
+     */
+    private String name;
+
+    /**
+     * 创建者名字
+     */
+    private String creatorName;
+
+    /**
+     * 域名
+     */
+    private String domainName;
+
+    /**
+     * 公司/企业名
+     */
+    private String companyName;
+
+    /**
+     * 组名
+     */
+    private String groupName;
+
+    /**
+     * 描述说明
+     */
+    private String description;
+
+    /**
+     * 操作次数
+     */
+    private Integer operateTimes;
+
+    /**
+     * 文件uri
+     */
+    private String uri;
+
+    /**
+     * 创建者地址
+     */
+    private String creatorAddress;
+
+    /**
+     * 创建时间
+     */
+    private String createTime;
+}
