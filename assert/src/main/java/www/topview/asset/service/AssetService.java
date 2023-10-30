82ab096d0fae4c25256cd01ab61ceaa4f6b1e9c2
lql <1964757607@qq.com>
2023-10-30 15:52:07 +0800

    feat(asset.service):完善createAsset方法

--- assert/src/main/java/www/topview/asset/service/AssetService.java
+++ assert/src/main/java/www/topview/asset/service/AssetService.java
@@ -1,5 +1,10 @@
 package www.topview.asset.service;
 
+import www.topview.asset.domain.dto.CreateAssetDTO;
+import www.topview.asset.domain.request.CreateAssetRequest;
+
+import java.io.IOException;
+
 /**
  * 资产service
  *
@@ -7,4 +12,10 @@ package www.topview.asset.service;
  * @date 2023/10/29
  */
 public interface AssetService {
+    /**
+     * 创建资产
+     * @param createAssetDTO 创建资产dto
+     * @return 创建成功返回true
+     */
+    Boolean createAsset(CreateAssetDTO createAssetDTO);
 }
