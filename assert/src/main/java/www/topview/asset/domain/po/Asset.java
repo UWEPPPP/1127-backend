c604303ac16fe914e222ea0474c623a2b6591ddc
lql <1964757607@qq.com>
2023-10-30 14:49:22 +0800

    feat(asset.po):完善Asset类

--- assert/src/main/java/www/topview/asset/domain/po/Asset.java
+++ assert/src/main/java/www/topview/asset/domain/po/Asset.java
@@ -14,8 +14,49 @@ import lombok.Data;
 @Data
 @TableName("tb_asset")
 public class Asset {
+    /**
+     * 主键
+     */
     @TableId(type = IdType.AUTO)
     private Long id;
 
+    /**
+     * 资产名字
+     */
+    private String name;
 
+    /**
+     * 创建者名字
+     */
+    private String creatorName;
+
+    /**
+     * 域名
+     */
+    private String domainName;
+
+    /**
+     * 公司/企业名
+     */
+    private String companyName;
+
+    /**
+     * 组名
+     */
+    private String groupName;
+
+    /**
+     * 描述说明
+     */
+    private String description;
+
+    /**
+     * 操作次数
+     */
+    private Integer operateTimes;
+
+    /**
+     * 资产状态（0 => 有效;1 => 无效;2 => 其他）
+     */
+    private Integer status;
 }
