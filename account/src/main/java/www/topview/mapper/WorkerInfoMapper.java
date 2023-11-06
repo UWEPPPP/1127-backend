a08be5b930ffadc7b91d14e772c77ecc076d2525
lql <1964757607@qq.com>
2023-11-06 14:25:02 +0800

    feat(mapper):添加通过weId搜索的方法

--- account/src/main/java/www/topview/mapper/WorkerInfoMapper.java
+++ account/src/main/java/www/topview/mapper/WorkerInfoMapper.java
@@ -1,5 +1,6 @@
 package www.topview.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Component;
 import www.topview.entity.po.WorkerInfo;
@@ -11,6 +12,13 @@ import www.topview.entity.po.WorkerInfo;
 
 @Component
 public interface WorkerInfoMapper extends BaseMapper<WorkerInfo> {
-
+    /**
+     * 通过weId搜索
+     * @param weId weid
+     * @return 返回信息
+     */
+    default WorkerInfo selectByWeId(String weId){
+        return this.selectOne(new QueryWrapper<WorkerInfo>().eq("weid", weId));
+    }
 
 }
